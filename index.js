e5e2bfd18ba54f3f5c7bed493f0b5773706e80fd
Newgen <reactnewgenjs@gmail.com>
2025-11-19 21:15:27 +0500

    express added

--- index.js
+++ index.js
@@ -30,3 +30,15 @@ bot.on("text", async (ctx) => {
 
 bot.launch();
 console.log("Bot started");
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
